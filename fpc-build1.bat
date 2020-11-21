a6f93f13077bc475b7d6f09461631bf74b79e4fa
Philip Sargent <philip.sargent@gmail.com>
2020-11-21 20:36:33 +0200

    File output not working under fpc
    Writing files crashes after some output

--- fpc-build1.bat
+++ fpc-build1.bat
@@ -1,7 +1,8 @@
-rem Using fpc 
-rem PMS 20-Nov-2020 11:10 
 echo off
+rem PMS 20-Nov-2020 20:35
+rem Using fpc 
 rem uses fpc.cfg to include these units:
+rem -FuC:\FPC\3.2.0\units\x86_64-win64\rtl-console -FuC:\FPC\3.2.0\units\x86_64-win64\rtl-extra -FuC:\FPC\3.2.0\units\x86_64-win64\graph
 rem - standard crt unit in rtl-console
 rem - standard printer unit in rtl-extra
 rem - standard graph unit in graph
@@ -10,6 +11,5 @@ rem should use a make utility as this will not recompile units
 del *.o
 del *.ppu
 
-C:\FPC\3.2.0\bin\i386-win32\ppcrossx64 -Mtp  dfm290 -ddebug -ddetail -dcontour
-rem -FuC:\FPC\3.2.0\units\x86_64-win64\rtl-console -FuC:\FPC\3.2.0\units\x86_64-win64\rtl-extra -FuC:\FPC\3.2.0\units\x86_64-win64\graph
+C:\FPC\3.2.0\bin\i386-win32\ppcrossx64 -Mtp  -g dfm290 
 
